cd78f1ab76065fb262ad937ed8f3a61b0bfb281c
Efanin <efanin.igor@gmail.com>
2024-11-25 20:52:04 +0300

    задание

--- WinFormsApp15/Form1.Designer.cs
+++ WinFormsApp15/Form1.Designer.cs
@@ -28,76 +28,138 @@
         /// </summary>
         private void InitializeComponent()
         {
-            textBox1 = new TextBox();
             comboBox1 = new ComboBox();
             button1 = new Button();
             label1 = new Label();
             comboBox2 = new ComboBox();
             monthCalendar1 = new MonthCalendar();
+            comboBox3 = new ComboBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
             SuspendLayout();
-            // 
-            // textBox1
-            // 
-            textBox1.Location = new Point(12, 27);
-            textBox1.Multiline = true;
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(257, 341);
-            textBox1.TabIndex = 0;
             // 
             // comboBox1
             // 
             comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(298, 135);
+            comboBox1.Location = new Point(24, 122);
             comboBox1.Name = "comboBox1";
             comboBox1.Size = new Size(257, 23);
             comboBox1.TabIndex = 1;
             // 
             // button1
             // 
-            button1.Location = new Point(603, 135);
+            button1.Location = new Point(208, 216);
             button1.Name = "button1";
-            button1.Size = new Size(75, 23);
+            button1.Size = new Size(73, 23);
             button1.TabIndex = 2;
-            button1.Text = "button1";
+            button1.Text = "Создать";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(298, 79);
+            label1.Font = new Font("Segoe UI", 14F);
+            label1.Location = new Point(311, 68);
             label1.Name = "label1";
-            label1.Size = new Size(38, 15);
+            label1.Size = new Size(63, 25);
             label1.TabIndex = 3;
-            label1.Text = "label1";
+            label1.Text = "Пусто";
             // 
             // comboBox2
             // 
             comboBox2.FormattingEnabled = true;
             comboBox2.Items.AddRange(new object[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" });
-            comboBox2.Location = new Point(561, 135);
+            comboBox2.Location = new Point(208, 176);
             comboBox2.Name = "comboBox2";
-            comboBox2.Size = new Size(36, 23);
+            comboBox2.Size = new Size(73, 23);
             comboBox2.TabIndex = 4;
             comboBox2.Text = "1";
             // 
             // monthCalendar1
             // 
-            monthCalendar1.Location = new Point(298, 187);
+            monthCalendar1.Location = new Point(24, 176);
             monthCalendar1.Name = "monthCalendar1";
             monthCalendar1.TabIndex = 5;
+            // 
+            // comboBox3
+            // 
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Location = new Point(24, 68);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(257, 23);
+            comboBox3.TabIndex = 6;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 14F);
+            label2.Location = new Point(24, 40);
+            label2.Name = "label2";
+            label2.Size = new Size(54, 25);
+            label2.TabIndex = 7;
+            label2.Text = "ФИО";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 14F);
+            label3.Location = new Point(24, 94);
+            label3.Name = "label3";
+            label3.Size = new Size(73, 25);
+            label3.TabIndex = 8;
+            label3.Text = "Деталь";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 14F);
+            label4.Location = new Point(208, 148);
+            label4.Name = "label4";
+            label4.Size = new Size(73, 25);
+            label4.TabIndex = 9;
+            label4.Text = "Кол-во";
+            label4.Click += label4_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 14F);
+            label5.Location = new Point(84, 148);
+            label5.Name = "label5";
+            label5.Size = new Size(53, 25);
+            label5.TabIndex = 10;
+            label5.Text = "Дата";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 16F, FontStyle.Bold);
+            label6.Location = new Point(311, 9);
+            label6.Name = "label6";
+            label6.Size = new Size(184, 30);
+            label6.TabIndex = 11;
+            label6.Text = "Новая продажа";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(847, 353);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(comboBox3);
             Controls.Add(monthCalendar1);
             Controls.Add(comboBox2);
             Controls.Add(label1);
             Controls.Add(button1);
             Controls.Add(comboBox1);
-            Controls.Add(textBox1);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
@@ -106,12 +168,16 @@
         }
 
         #endregion
-
-        private TextBox textBox1;
         private ComboBox comboBox1;
         private Button button1;
         private Label label1;
         private ComboBox comboBox2;
         private MonthCalendar monthCalendar1;
+        private ComboBox comboBox3;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
     }
 }
--- WinFormsApp15/Form1.cs
+++ WinFormsApp15/Form1.cs
@@ -5,16 +5,23 @@ namespace WinFormsApp15
     {
         private SqlConnection connection;
         private List<Item> items;
+        private List<Item> itemscust;
         public Form1()
         {
             InitializeComponent();
-            connection = new("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\user\\source\\repos\\WinFormsApp15\\WinFormsApp15\\Database1.mdf;Integrated Security=True");
+            connection = new("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\user\\Source\\Repos\\SqlWinForms\\WinFormsApp15\\Database1.mdf;Integrated Security=True");
             items = new();
+            itemscust = new();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             connection.Open();
+            table1();
+            table2();
+        }
+        private void table1()
+        {
             SqlCommand command = new(
                 "select * from details",
                 connection
@@ -33,20 +40,46 @@ namespace WinFormsApp15
             reader.Close();
             foreach (var item in items)
             {
-                textBox1.Text += $"{item.id.ToString()} {item.name} {item.price.ToString()} \n";
                 comboBox1.Items.Add(item.name);
+            }
+        }
+        private void table2()
+        {
+            SqlCommand command = new(
+                "select * from customers",
+                connection
+                );
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                itemscust.Add(
+                    new Item(
+                            Convert.ToInt32(reader["id"]),
+                            Convert.ToString(reader["name"])
+                        )
+                    );
+            }
+            reader.Close();
+            foreach (var item in itemscust)
+            {
+                comboBox3.Items.Add(item.name);
             }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            string date = monthCalendar1.SelectionStart.ToShortDateString();
             int total = int.Parse(comboBox2.Text) * items[comboBox1.SelectedIndex].price;
-            label1.Text = items[comboBox1.SelectedIndex].name +
-                " " + total.ToString()+"póá.";
-            MessageBox.Show(monthCalendar1.SelectionStart.Date.ToString());
+            label1.Text = itemscust[comboBox3.SelectedIndex].name + " | " + items[comboBox1.SelectedIndex].name +
+                " " + total.ToString() + "póá.  " + date;
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
         }
     }
-    public class Item(int id,string name, int price)
+    public class Item(int id,string name, int price = 0)
     {
         public int id=id;
         public string name=name;
