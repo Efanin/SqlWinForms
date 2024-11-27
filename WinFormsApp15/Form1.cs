c47682bbdd3a98e4d8acf42ee14e1c3f53c58d24
Efanin <efanin.igor@gmail.com>
2024-11-27 21:23:39 +0300

    дата

--- WinFormsApp15/Form1.cs
+++ WinFormsApp15/Form1.cs
@@ -9,7 +9,7 @@ namespace WinFormsApp15
         public Form1()
         {
             InitializeComponent();
-            connection = new("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\user\\Source\\Repos\\SqlWinForms\\WinFormsApp15\\Database1.mdf;Integrated Security=True");
+            connection = new("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\user\\source\\repos\\WinFormsApp15\\WinFormsApp15\\Database1.mdf;Integrated Security=True");
             items = new();
             itemscust = new();
         }
@@ -43,6 +43,7 @@ namespace WinFormsApp15
                 comboBox1.Items.Add(item.name);
             }
         }
+
         private void table2()
         {
             SqlCommand command = new(
@@ -68,15 +69,66 @@ namespace WinFormsApp15
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string date = monthCalendar1.SelectionStart.ToShortDateString();
-            int total = int.Parse(comboBox2.Text) * items[comboBox1.SelectedIndex].price;
-            label1.Text = itemscust[comboBox3.SelectedIndex].name + " | " + items[comboBox1.SelectedIndex].name +
+            string namefio;
+            string detail;
+            int total;
+            int price;
+            string date;
+            if (comboBox3.SelectedIndex == -1)
+                namefio = newNameFio(comboBox3.Text);
+            else
+                namefio = itemscust[comboBox3.SelectedIndex].name;
+            if (comboBox1.SelectedIndex == -1)
+            {
+                detail = newDetail(comboBox1.Text, out price);
+                total = int.Parse(comboBox2.Text) * price;
+            }
+            else
+            {
+                detail = items[comboBox1.SelectedIndex].name;
+                total = int.Parse(comboBox2.Text) * items[comboBox1.SelectedIndex].price;
+            }    
+            date = monthCalendar1.SelectionStart.ToShortDateString();
+            label1.Text = namefio + " | " + detail +
                 " " + total.ToString() + "póá.  " + date;
         }
 
+        private string newNameFio(string name)
+        {
+            FormNewNameFio formNewNameFio = new(name,this);
+            formNewNameFio.Show();
+            return name;
+        }
+        private string newDetail(string name, out int price)
+        {
+            FormNewDetail formNewDetail = new(name,this);
+            formNewDetail.Show();
+            price = 0;
+            return name;
+        }
         private void label4_Click(object sender, EventArgs e)
         {
 
+        }
+        public void addNewNameFio(string nameFio)
+        {
+            SqlCommand command = new(
+                $"insert into [customers] (name) values (N'{nameFio}')",
+                connection
+                );
+            command.ExecuteNonQuery();
+            itemscust.Clear();
+            table2();
+        }
+        public void addNewDetail(string detail, int price)
+        {
+            SqlCommand command = new(
+               $"insert into [detail] (name,price) values (N'{detail}','{price}')",
+               connection
+               );
+            command.ExecuteNonQuery();
+            items.Clear();
+            table1();
         }
     }
     public class Item(int id,string name, int price = 0)
---
+++ WinFormsApp15/FormNewDetail.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp15
+{
+    public partial class FormNewDetail : Form
+    {
+        string detail;
+        Form1 form1;
+        public FormNewDetail(string detail, Form1 form1)
+        {
+            InitializeComponent();
+            this.detail = detail;
+            this.form1 = form1;
+        }
+
+        private void FormNewDetail_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int price = int.Parse(textBox1.Text);
+            form1.addNewDetail(detail, price);
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
